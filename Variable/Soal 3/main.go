90c0654c4681f0774d40b714a7652998ffc6d990
Jacobus <jakobusss44@gmail.com>
2024-01-16 16:54:48 +0700

    tugas flowchart

---
+++ Variable/Soal 3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "halo"
+	umur := 1
+
+	fmt.Println("Hello world")
+	fmt.Println(a, umur)
+
+	var hargatelor uint16     // 0
+	var umurfawwaz uint8 = 99 //50
+	var umurjerry uint8 = 99
+	var komakoma float32
+	var namaku string
+
+	fmt.Println(hargatelor, umurfawwaz, namaku, komakoma)
+
+	var hasilperkalian = uint16(umurjerry) * uint16(umurfawwaz)
+	fmt.Println(hasilperkalian)
+
+}
